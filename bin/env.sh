98744aeb459b298f4f9b6fdc28198bbba015f330
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-01-05 09:11:48 -0600

    Simplify setting env variables
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- bin/env.sh
+++ bin/env.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
-declare -A IMAGES=(
-    ["volume"]="edgexfoundry/docker-edgex-volume"
-    ["consul"]="edgexfoundry/docker-core-consul"
-    ["coreMetadata"]="edgexfoundry/docker-core-metadata"
-    ["mongo"]="edgexfoundry/docker-edgex-mongo"
-    ["mongoSeed"]="edgexfoundry/docker-edgex-mongo-seed"
-    ["supportLogging"]="edgexfoundry/docker-support-logging"
-    ["coreMetadata"]="edgexfoundry/docker-core-metadata"
-    ["coreData"]="edgexfoundry/docker-core-data"
-    ["coreCommand"]="edgexfoundry/docker-core-command"
-    ["supportScheduler"]="edgexfoundry/docker-support-scheduler"
-    ["exportClient"]="edgexfoundry/docker-export-client"
-    ["exportDistro"]="edgexfoundry/docker-export-distro"
-    ["supportRulesengine"]="edgexfoundry/docker-support-rulesengine"
-    ["deviceVirtual"]="edgexfoundry/docker-device-virtual"
-)
+export volume=edgexfoundry/docker-edgex-volume
+export consul=edgexfoundry/docker-core-consul
+export coreMetadata=edgexfoundry/docker-core-metadata
+export mongo=edgexfoundry/docker-edgex-mongo
+export mongoSeed=edgexfoundry/docker-edgex-mongo-seed
+export supportLogging=edgexfoundry/docker-support-logging
+export coreMetadata=edgexfoundry/docker-core-metadata
+export coreData=edgexfoundry/docker-core-data
+export coreCommand=edgexfoundry/docker-core-command
+export supportScheduler=edgexfoundry/docker-support-scheduler
+export exportClient=edgexfoundry/docker-export-client
+export exportDistro=edgexfoundry/docker-export-distro
+export supportRulesengine=edgexfoundry/docker-support-rulesengine
+export deviceVirtual=edgexfoundry/docker-device-virtual
 
-echo "TEST_SERVICE : ${TEST_SERVICE}"
-echo "TEST_SERVICE replace default"
-IMAGES["${TEST_SERVICE}"]="${TEST_SERVICE_IMAGE}"
-
-
-for image in "${!IMAGES[@]}";
-do
-    echo "set env ${image} ${IMAGES[$image]}"
-    export $image=${IMAGES[$image]}
-done
-
-echo "~~~~~~~~~~~~~~~~~!!!!!!!!!!!!"
-echo "${coreMetadata}"
-echo "${volume}"
-echo "${consul}"
