d19b04daa9f33f417ca394ff104e644db4a98a5a
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-05-16 07:23:31 -0500

    Test 0.2.1 tags from nexus3
    This runs the barcelona release
    through the integration tests.
    
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- bin/env.sh
+++ bin/env.sh
@@ -2,15 +2,15 @@
 
 export volume=edgexfoundry/docker-edgex-volume
 export consul=edgexfoundry/docker-core-consul
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go
+export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata:0.2.1
 export mongo=edgexfoundry/docker-edgex-mongo
 export mongoSeed=edgexfoundry/docker-edgex-mongo-seed
-export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging
-export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go
-export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go
-export supportScheduler=edgexfoundry/docker-support-scheduler
-export exportClient=edgexfoundry/docker-export-client
-export exportDistro=edgexfoundry/docker-export-distro
-export supportRulesengine=edgexfoundry/docker-support-rulesengine
-export deviceVirtual=edgexfoundry/docker-device-virtual
+export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging:0.2.1
+export coreData=nexus3.edgexfoundry.org:10004/docker-core-data:0.2.1
+export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command:0.2.1
+export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler:0.2.1
+export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client:0.2.1
+export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro:0.2.1
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine:0.2.1
+export deviceVirtual=nexus3.edgexfoundry.org:10004/docker-device-virtual:0.2.1
 
