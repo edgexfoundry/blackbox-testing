7214ea15c35ad2d3891b065efe140b7ce55aba25
Bruce <weichou1229@gmail.com>
2018-06-28 00:47:34 +0800

    init export-client-test
    Signed-off-by: Bruce <weichou1229@gmail.com>

---
+++ bin/importExportClientDataDump.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NAMESFILE=$(dirname "$0")/files.sh
+
+if [ -f $NAMESFILE ]; then 
+
+	. $NAMESFILE
+
+else 
+	echo "Error: Names file does not exist."
+	exit $?
+
+fi
+
+DATA_BASE="exportclient"
+COLLECTIONS=( "exportConfiguration" )
+DUMP_FILES=( $REGISTERDATADUMP)
+
+for index in "${!DUMP_FILES[@]}"
+do
+    docker-compose exec -T mongo /bin/bash -c "mongoimport -d ${DATA_BASE} -c ${COLLECTIONS[index]} --file ${DUMP_FILES[index]}"
+
+    echo "Info: ${DUMP_FILES[index]} data imported"
+done
