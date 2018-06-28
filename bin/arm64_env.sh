8860475bae5a94a4bc2eb5faa5ac25d70d6f4978
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-06-28 09:38:02 -0500

    Add missing mongoSeed variable
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -5,6 +5,7 @@ export consul=consul:1.1.0
 export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
 export coreConfigseed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go-arm64:master
 export mongo=edgexfoundry/docker-edgex-mongo
+export mongoSeed=edgexfoundry/docker-edgex-mongo-seed
 export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go-arm64
 export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go-arm64
 export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64
