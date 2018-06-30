70feb93b1a579e0c36c2bd2be0b61759bd5814cd
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-06-29 22:00:42 -0500

    Set container env to correct latest staging images in nexus
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-export volume=edgexfoundry/docker-edgex-volume
-export consul=consul:1.1.0
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
+export volume=nexus3.edgexfoundry.org:10004/docker-edgex-volume-arm64:master
+export consul=nexus3.edgexfoundry.org:10001/arm64v8/consul:1.1.0
+export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64:0.5.2
 export coreConfigseed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go-arm64:master
-export mongo=edgexfoundry/docker-edgex-mongo
-export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go-arm64
-export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go-arm64
-export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64
-export supportScheduler=edgexfoundry/docker-support-scheduler
-export exportClient=edgexfoundry/docker-export-client-go-arm64
-export exportDistro=edgexfoundry/docker-export-distro-go-arm64
-export supportRulesengine=edgexfoundry/docker-support-rulesengine
-export deviceVirtual=edgexfoundry/docker-device-virtual
+export mongo=nexus3.edgexfoundry.org:10004/docker-edgex-mongo-arm64:master
+export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go-arm64:0.5.2
+export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go-arm64:0.5.2
+export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64:0.5.2
+export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler-arm64:0.5.0
+export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go-arm64:0.5.2
+export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go-arm64:0.5.2
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine-arm64:0.5.0
+export deviceVirtual=nexus3.edgexfoundry.org:10004/docker-device-virtual-arm64:0.5.0
+export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-arm64:0.5.0
+export postman=nexus3.edgexfoundry.org:10004/postman-newman-arm64:3.9.4
 
