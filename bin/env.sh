a143dfdedfd43f3a1a455f3fcbd7f0326227dbcb
Bruce <weichou1229@gmail.com>
2018-07-03 22:14:15 +0800

    update image version
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/env.sh
+++ bin/env.sh
@@ -2,16 +2,21 @@
 
 export volume=edgexfoundry/docker-edgex-volume
 export consul=consul:1.1.0
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go
-export coreConfigseed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go:master
+export coreConfigseed=edgexfoundry/docker-core-config-seed-go
+
 export mongo=edgexfoundry/docker-edgex-mongo
 export mongoSeed=edgexfoundry/docker-edgex-mongo-seed
-export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging
-export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go
-export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go
+
+export coreData=edgexfoundry/docker-core-data-go
+export coreMetadata=edgexfoundry/docker-core-metadata-go
+export coreCommand=edgexfoundry/docker-core-command-go
+
+export supportLogging=edgexfoundry/docker-support-logging-go
+export supportNotification=edgexfoundry/docker-support-notifications
 export supportScheduler=edgexfoundry/docker-support-scheduler
-export exportClient=edgexfoundry/docker-export-client
-export exportDistro=edgexfoundry/docker-export-distro
 export supportRulesengine=edgexfoundry/docker-support-rulesengine
+
 export deviceVirtual=edgexfoundry/docker-device-virtual
 
+export exportClient=edgexfoundry/docker-export-client-go
+export exportDistro=edgexfoundry/docker-export-distro-go
