619563645b87c6d9c589b4b068026f4442ebe216
Bruce <weichou1229@gmail.com>
2018-07-08 22:32:14 +0800

    latest image not exist , so specify 0.6.0 as supportNotifications, supportScheduler, supportRulesengine image version
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -10,9 +10,9 @@ export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
 export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64
 
 export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go-arm64
-export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-arm64
-export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler-arm64
-export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine-arm64
+export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-arm64:0.6.0
+export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler-arm64:0.6.0
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine-arm64:0.6.0
 
 export exportClient=edgexfoundry/docker-export-client
 export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go-arm64
