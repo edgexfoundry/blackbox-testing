8f2bf45a23a0d1652f2019e60130911877592215
Bruce <weichou1229@gmail.com>
2018-07-08 19:32:49 +0800

    migrate docker-compose file to california 0.6.0
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -2,15 +2,19 @@
 
 export volume=edgexfoundry/docker-edgex-volume
 export consul=consul:1.1.0
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
-export coreConfigseed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go-arm64:master
+export configSeed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go-arm64:master
 export mongo=edgexfoundry/docker-edgex-mongo
-export supportLogging=edgexfoundry/docker-support-logging-go
+
 export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go-arm64
+export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
 export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64
+
+export supportLogging=edgexfoundry/docker-support-logging-go
+export supportNotifications=edgexfoundry/docker-support-notifications
 export supportScheduler=edgexfoundry/docker-support-scheduler
+export supportRulesengine=edgexfoundry/docker-support-rulesengine
+
 export exportClient=edgexfoundry/docker-export-client-go-arm64
 export exportDistro=edgexfoundry/docker-export-distro-go-arm64
-export supportRulesengine=edgexfoundry/docker-support-rulesengine
 export deviceVirtual=edgexfoundry/docker-device-virtual
 
