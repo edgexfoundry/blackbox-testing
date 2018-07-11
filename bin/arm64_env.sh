ed59b99e297e176f2f12e1bfccb368c03c8a449e
Bruce <weichou1229@gmail.com>
2018-07-12 00:25:26 +0800

    migrate to california 0.6.0
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-export volume=nexus3.edgexfoundry.org:10004/docker-edgex-volume:master
-export consul=consul:1.1.0
-export configSeed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go
-export mongo=nexus3.edgexfoundry.org:10004/docker-edgex-mongo:master
+export volume=nexus3.edgexfoundry.org:10004/docker-edgex-volume-arm64:master
+export consul=nexus3.edgexfoundry.org:10001/arm64v8/consul:1.1.0
+export configSeed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go-arm64
+export mongo=nexus3.edgexfoundry.org:10004/docker-edgex-mongo-arm64:master
 
-export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go
-export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go
+export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go-arm64
+export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go-arm64
+export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go-arm64
 
-export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go
-export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications
-export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler
-export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine
+export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go-arm64
+export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-arm64:0.6.0
+export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler-arm64:0.6.0
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine-arm64:0.6.0
 
-export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go
-export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go
-export deviceVirtual=nexus3.edgexfoundry.org:10004/docker-device-virtual
+export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go-arm64
+export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go-arm64
+export deviceVirtual=nexus3.edgexfoundry.org:10004/docker-device-virtual-arm64
