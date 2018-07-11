c42e22851779fda02c60a8dd5f89e0f84eb96e04
Bruce <weichou1229@gmail.com>
2018-07-12 00:12:09 +0800

    migrate to california 0.6.0
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- deploy-edgeX.sh
+++ deploy-edgeX.sh
@@ -42,7 +42,6 @@ run_service mongo
 
 sleep 10
 
-run_service mongo-seed
 run_service logging
 
 while ! $(docker-compose exec -T consul nc -z edgex-support-logging 48061);do echo "not already startup… wait for 5 second reconnect." ;sleep 5; done
@@ -78,10 +77,10 @@ run_service export-client
 while ! $(docker-compose exec -T consul nc -z edgex-export-client 48071);do echo "not already startup… wait for 5 second reconnect." ;sleep 5; done
 sleep 5
 
-#run_service export-distro
+run_service export-distro
 
-#while ! $(docker-compose exec -T consul nc -z edgex-export-distro 48070);do echo "not already startup… wait for 5 second reconnect." ;sleep 5; done
-#sleep 5
+while ! $(docker-compose exec -T consul nc -z edgex-export-distro 48070);do echo "not already startup… wait for 5 second reconnect." ;sleep 5; done
+sleep 5
 
 #run_service rulesengine
 
