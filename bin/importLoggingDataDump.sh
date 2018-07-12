3cb9ce11e5da93dc5ee0cd5738206496d0c1bf0c
Bruce <weichou1229@gmail.com>
2018-07-12 23:26:41 +0800

    change dump data to json file
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/importLoggingDataDump.sh
+++ bin/importLoggingDataDump.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-DATA_BASE="logging"
+NAMESFILE=$(dirname "$0")/files.sh
 
-DUMP_FILES=(
-    "/etc/newman/DataDumps/logging/logEntry.js"
-)
+if [ -f $NAMESFILE ]; then
+
+	. $NAMESFILE
+
+else
+	echo "Error: Names file does not exist."
+	exit $?
+
+fi
+
+DATA_BASE="logging"
+COLLECTIONS=( "logEntry" )
+DUMP_FILES=( $LOGGINGDATADUMP )
 
 for index in "${!DUMP_FILES[@]}"
 do
-    docker-compose exec -T mongo /bin/bash -c "mongo ${DATA_BASE} ${DUMP_FILES[index]}"
+    docker-compose exec -T mongo /bin/bash -c "mongoimport -d ${DATA_BASE} -c ${COLLECTIONS[index]} --file ${DUMP_FILES[index]}"
+
+    echo "Info: ${DUMP_FILES[index]} data imported"
 done
--- bin/postman-test/DataDumps/logging/populate.sh
+++ bin/postman-test/DataDumps/logging/populate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mongo logging logEntry.js
+mongoimport -d logging -c logEntry --file logEntryDb.json
