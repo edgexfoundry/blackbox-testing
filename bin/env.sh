f9e0713cd91fdd677da2d2c5a94530148a6ab5c1
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-10-09 17:50:47 -0500

    Update rulesengine to 0.7.0 tag
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- bin/env.sh
+++ bin/env.sh
@@ -12,7 +12,7 @@ export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go:0.7.0
 export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go:0.7.0
 export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-go:0.7.0
 export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler:0.6.0
-export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine:0.6.0
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine:0.7.0
 
 export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go:0.7.0
 export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go:0.7.0
