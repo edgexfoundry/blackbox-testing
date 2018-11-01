71916b0c3a95203e18f51ae189e793bbcbeadecb
Felix Ting <felix@iotechsys.com>
2018-11-01 21:50:58 +0800

    Update versions to 1.0.0 in environment scripts
    Signed-off-by: Felix Ting <felix@iotechsys.com>

--- bin/env.sh
+++ bin/env.sh
@@ -2,19 +2,19 @@
 
 export volume=nexus3.edgexfoundry.org:10004/docker-edgex-volume:0.6.0
 export consul=consul:1.1.0
-export configSeed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go:0.7.0
+export configSeed=nexus3.edgexfoundry.org:10004/docker-core-config-seed-go:1.0.0
 export mongo=nexus3.edgexfoundry.org:10004/docker-edgex-mongo:0.6.0
 
-export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go:0.7.0
-export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go:0.7.0
-export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go:0.7.0
+export coreData=nexus3.edgexfoundry.org:10004/docker-core-data-go:1.0.0
+export coreMetadata=nexus3.edgexfoundry.org:10004/docker-core-metadata-go:1.0.0
+export coreCommand=nexus3.edgexfoundry.org:10004/docker-core-command-go:1.0.0
 
-export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go:0.7.0
-export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-go:0.7.0
-export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler:0.6.0
-export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine:0.7.0
+export supportLogging=nexus3.edgexfoundry.org:10004/docker-support-logging-go:1.0.0
+export supportNotifications=nexus3.edgexfoundry.org:10004/docker-support-notifications-go:1.0.0
+export supportScheduler=nexus3.edgexfoundry.org:10004/docker-support-scheduler-go:1.0.0
+export supportRulesengine=nexus3.edgexfoundry.org:10004/docker-support-rulesengine:1.0.0
 
-export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go:0.7.0
-export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go:0.7.0
+export exportClient=nexus3.edgexfoundry.org:10004/docker-export-client-go:1.0.0
+export exportDistro=nexus3.edgexfoundry.org:10004/docker-export-distro-go:1.0.0
 export deviceVirtual=nexus3.edgexfoundry.org:10004/docker-device-virtual:0.6.0
 export postman=postman/newman_ubuntu1404:3.10.0
