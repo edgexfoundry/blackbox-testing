b9759a2e91794f6f2e57e82aa9181767491a861b
Bruce <weichou1229@gmail.com>
2018-12-14 15:38:28 +0800

    Rename env file
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/importCoreDataDump.sh
+++ bin/importCoreDataDump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 COLLECTION_PATH="collections/core-data-importer.postman_collection.json"
-ENV_PATH="environment/CoredataEnv.postman_environment.json"
+ENV_PATH="environment/core-data-docker.postman_environment.json"
 
 
 echo "Info: import Coredata's test data."
