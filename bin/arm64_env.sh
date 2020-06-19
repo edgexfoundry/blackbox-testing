d2a94660c87ecd31e8909df58c9b25de4913defa
Ginny Guan <ginny@iotechsys.com>
2020-06-19 11:46:04 +0800

    Upgrade app service version in geneva branch
    Fix https://github.com/edgexfoundry/blackbox-testing/issues/460
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- bin/arm64_env.sh
+++ bin/arm64_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export appService=nexus3.edgexfoundry.org:10004/docker-app-service-configurable-arm64:master
+export appService=edgexfoundry/docker-app-service-configurable-arm64:1.2.0
 export postman=nexus3.edgexfoundry.org:10003/edgex-newman:4.5.6-arm64
 export docker_compose_test_tools=$PWD/docker-compose-test-tools.yml
 
--- bin/env.sh
+++ bin/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export appService=nexus3.edgexfoundry.org:10004/docker-app-service-configurable:master
+export appService=edgexfoundry/docker-app-service-configurable:1.2.0
 export postman=postman/newman:4.5.6
 export docker_compose_test_tools=$PWD/docker-compose-test-tools.yml
 
