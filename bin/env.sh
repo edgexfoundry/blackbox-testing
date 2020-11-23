2aa71fbf2b24cade5a9756a87597995608d301f0
Ginny Guan <ginny@iotechsys.com>
2020-11-23 15:12:08 +0800

    Revert "Update to use Hanoi release images" to use nightly-build images
    This reverts commit 42b621f126e8332415004967021937796bc98796.
    
    Fix https://github.com/edgexfoundry/blackbox-testing/issues/472

--- bin/env.sh
+++ bin/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export appService=edgexfoundry/docker-app-service-configurable:1.3.0
+export appService=nexus3.edgexfoundry.org:10004/docker-app-service-configurable:master
 export postman=postman/newman:4.5.6
 export docker_compose_test_tools=$PWD/docker-compose-test-tools.yml
 
